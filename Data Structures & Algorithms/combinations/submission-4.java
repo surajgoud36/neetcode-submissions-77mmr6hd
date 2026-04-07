7f8e6c704d1d668a7f7ec96d5b3784041b3f2624
Suraj Goud <112563526+surajgoud36@users.noreply.github.com>
2026-04-07 16:37:59 +0530

    Add: combinations - submission-4

---
+++ Data Structures & Algorithms/combinations/submission-4.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res= new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        helper(n,k,1,path,res);
+        return res;
+    }
+    public void helper(int n,int k,int i,List<Integer> path,List<List<Integer>> res){
+        if(path.size()==k){
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        if(i>n)
+            return;
+        
+        for(int j=i;j<=n;j++){
+            path.add(j);
+            helper(n,k,j+1,path,res);
+            path.remove(Integer.valueOf(j));
+        }
+    }
+}
