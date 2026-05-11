9d0052c4b7c2dfde00ffb330cae0cc386328f9a2
Suraj Goud <112563526+surajgoud36@users.noreply.github.com>
2026-05-11 09:01:10 +0530

    Add: sql-the-latest-login-in-2020 - submission-2

---
+++ SQL/sql-the-latest-login-in-2020/submission-2.sql
@@ -0,0 +1,4 @@
+-- Write your query below
+select distinct on(user_id) user_id,time_stamp as last_stamp
+from logins where extract(year from time_stamp::timestamp) = 2020
+order by user_id,time_stamp desc;
